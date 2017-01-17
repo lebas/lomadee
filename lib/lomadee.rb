b3f5f7602bb2e5607aa2b84d3d17ac45dd2d75d1
Eduardo <eduardo@MacBook-Pro-de-Eduardo.local>
2017-01-16 23:55:46 -0200

    [FIXED] UTF8 to ASCII in search keyword

--- lib/lomadee.rb
+++ lib/lomadee.rb
@@ -57,7 +57,7 @@ module Lomadee
 
     def get_products_with_keyword(keyword = nil)
       prod = []
-      unless @server_url.nil? || keyword.ascii_only?
+      if !@server_url.nil? && keyword.ascii_only?
         url_page = "#{@server_url}service/findProductList/lomadee/#{@api_id}/BR/?sourceId=#{@source_id}&keyword=#{keyword.downcase}"
         @page = Nokogiri::XML(open(url_page))
         if !@page.nil? && !@page.css("details").css("status").nil? && @page.css("details").css("status").text == "success"
